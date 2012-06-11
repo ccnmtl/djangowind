3f762982f494183c8c783d82745ca9196bbdab25
Anders Pearson <anders@columbia.edu>
2012-06-11 16:44:59 -0400

    flake8 cleanup and warn if ldap is not installed

--- djangowind/urls.py
+++ djangowind/urls.py
@@ -1,13 +1,19 @@
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import patterns
 
 urlpatterns = patterns('',
                        (r'^login/$', 'djangowind.views.login'),
-                       (r'^windlogin/$','djangowind.views.windlogin'),
-                       (r'^logout/$','djangowind.views.logout'),
-                       (r'^logout_then_login/$','django.contrib.auth.views.logout_then_login'),
-                       (r'^redirect_to_login/$','django.contrib.auth.views.redirect_to_login'),
-                       (r'^password_reset/$','django.contrib.auth.views.password_reset'),
-                       (r'^password_reset_done/$','django.contrib.auth.views.password_reset_done'),
-                       (r'^password_change/$','django.contrib.auth.views.password_change'),
-                       (r'^password_change_done/$','django.contrib.auth.views.password_change_done')
+                       (r'^windlogin/$', 'djangowind.views.windlogin'),
+                       (r'^logout/$', 'djangowind.views.logout'),
+                       (r'^logout_then_login/$',
+                        'django.contrib.auth.views.logout_then_login'),
+                       (r'^redirect_to_login/$',
+                        'django.contrib.auth.views.redirect_to_login'),
+                       (r'^password_reset/$',
+                        'django.contrib.auth.views.password_reset'),
+                       (r'^password_reset_done/$',
+                        'django.contrib.auth.views.password_reset_done'),
+                       (r'^password_change/$',
+                        'django.contrib.auth.views.password_change'),
+                       (r'^password_change_done/$',
+                        'django.contrib.auth.views.password_change_done')
                        )
