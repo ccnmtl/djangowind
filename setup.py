30261d883b25e35822a0d601cbb7ab3f98b42a36
Anders Pearson <anders@columbia.edu>
2016-12-06 06:55:52 +0000

    :bookmark: release 0.16.1

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="djangowind",
-    version="0.16.0",
+    version="0.16.1",
     author="Anders Pearson",
     author_email="anders@columbia.edu",
     url="https://github.com/ccnmtl/djangowind",
